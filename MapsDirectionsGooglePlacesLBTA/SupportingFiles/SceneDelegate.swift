e68b95e3e70c502c89b69efc016a37ab7a5c5b96
mohamed <mohamedibrahim1223@gmail.com>
2021-05-31 15:17:08 +0300

    adding gitignore.

---
+++ MapsDirectionsGooglePlacesLBTA/SupportingFiles/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  MapsDirectionsGooglePlacesLBTA
+//
+//  Created by MIF50 on 31/05/2021.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let contentView = ContentView()
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+//            window.rootViewController = UIHostingController(rootView: contentView)
+            window.rootViewController = MainVC()
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+}
+
---
+++ MapsDirectionsGooglePlacesLBTA/UI/MainVC.swift
@@ -0,0 +1,56 @@
+//
+//  MainVC.swift
+//  MapsDirectionsGooglePlacesLBTA
+//
+//  Created by MIF50 on 31/05/2021.
+//
+
+import UIKit
+import MapKit
+import LBTATools
+
+class MainVC: UIViewController {
+    
+    // MARK:- Views
+    let mapView = MKMapView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureMap()
+        configureRegionForMap()
+    }
+    
+    private func configureMap() {
+        view.addSubview(mapView)
+        mapView.fillSuperview()
+        mapView.mapType = .standard
+    }
+    
+    private func configureRegionForMap() {
+        let centerCoordinate = CLLocationCoordinate2D(latitude: 37.7666, longitude: -122.427290)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: centerCoordinate, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+}
+
+// MARK:- SwiftUI Preview
+import SwiftUI
+
+struct MainPreview: PreviewProvider {
+    static var previews: some View {
+        ContainerView()
+            .edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        func makeUIViewController(context: Context) -> MainVC {
+            return MainVC()
+        }
+        
+        func updateUIViewController(_ uiViewController: MainVC, context: Context) {}
+        
+        typealias UIViewControllerType = MainVC
+    }
+}
